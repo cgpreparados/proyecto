3a9fdeda3b1a4486d59fa70a82ef3134b36c6299
cgpreparados <cgpreparados@gmail.com>
2021-07-17 17:23:08 -0400

    Update sidebar.blade.php

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -243,7 +243,7 @@
             @endif
             @if((auth()->user()->type) == 1 )
             <li class="@if ($activePage == 'register') active @endif">
-                <a href="/register">
+                <a href="{{ route('register') }}">
                     <i class="now-ui-icons ui-1_send"></i>
                     <p> {{ __("Registrar Usuario") }} </p>
                 </a>
