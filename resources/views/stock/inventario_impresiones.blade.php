459e2a5099786202f2cbd4b897a36bb90b1103f2
cgpreparados <cgpreparados@gmail.com>
2022-02-24 00:06:19 -0300

    Update inventario_impresiones.blade.php

--- resources/views/stock/inventario_impresiones.blade.php
+++ resources/views/stock/inventario_impresiones.blade.php
@@ -47,6 +47,7 @@
                             <option value="Lotes">Inventario Lotes</option>
                             <option value="Alta">Alta</option>
                             <option value="Baja">Baja</option>
+                            <option value="Desperdicio">Desperdicio</option>
                         </select>
                     </div>
                     <div class="col-md-2" style='float:left; display:inline-block'>
